95bc9c0ec50d5fab3ba1fb03b89db5e08e29ba66
toorajjahangiri <toorajjahangiri@gmail.com>
2021-06-20 18:50:13 +0430

    Pakage __init__.py Created

---
+++ __init__.py
@@ -0,0 +1,9 @@
+###########################################
+__author__ = "ToorajJahangiri"
+__email__ = "Toorajjahangiri@gmail.com"
+###########################################
+
+# IMPORT
+from . execline import Signal, ExeLine
+
+__all__ = ("Signal", "ExeLine")
